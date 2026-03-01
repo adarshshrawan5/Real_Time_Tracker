a7ff36d81f13dcce2a2339abd01fec7a4207bcbf
Adarsh Shrawan Kumar Yadav <adarshshrawan5@gmail.com>
2026-03-01 11:58:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const http = require('http');
+
+const sockit = require('socket.io');
+const server = http.createServer(app);
+const io = sockit(server);
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection",function (socket){
+    socket.on("send-location",function (data){
+        io.emit("receive-location",{id:socket.id, ...data});
+    });
+
+    socket.on("disconnect", function () {
+        io.emit("user-disconnected", socket.id);
+    })
+    
+});
+
+app.get("/",function (req,res) {
+    res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, "0.0.0.0" , function(){
+    const actualPort = server.address() && server.address().port;
+    console.log(`Server running on http://localhost:${actualPort}`);
+});
+
---
+++ public/js/script.js
@@ -0,0 +1,48 @@
+const socket = io();
+
+if (navigator.geolocation) {
+navigator.geolocation.watchPosition(
+    (position) => {
+        const { latitude, longitude, accuracy } = position.coords;
+
+        console.log("Accuracy:", accuracy, "meters");
+
+        socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+        console.error(error);
+    },
+    {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0
+    }
+);
+} 
+
+const map = L.map('map').setView([0, 0],15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution :"Adarsh Map"
+}).addTo(map)
+
+const markers = {};
+
+socket.on("receive-location",  (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude],15);
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    }
+    else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+
+});                                      
+
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
